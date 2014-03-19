1205caa1da48e36479bd957750221d060c825875
wvengen <dev-voko@willem.engen.nl>
2014-03-19 22:21:57 +0100

    fix signup spec (complements 15a8f83c2df71583657eac256d46967b4c533cfb)

--- lib/foodsoft_signup/app/controllers/signup_controller.rb
+++ lib/foodsoft_signup/app/controllers/signup_controller.rb
@@ -9,33 +9,34 @@ class SignupController < ApplicationController
       redirect_to root_url, alert: I18n.t('signup.controller.disabled', foodcoop: FoodsoftConfig[:name])
     elsif FoodsoftConfig[:signup] != true and params[:key] != FoodsoftConfig[:signup]
       redirect_to root_url, alert: I18n.t('signup.controller.key_wrong', foodcoop: FoodsoftConfig[:name])
-    end
-    @user = User.new(params[:user])
-    if request.post?
-      begin
-        # XXX code-duplication from LoginController#accept_invitation
-        User.transaction do
-          # enforce group (security!)
-          @user.ordergroup = {id: 'new'}
-          # save!
-          if @user.save
-            session[:locale] = @user.locale
-            # but we proceed slightly differently (TODO same behaviour for invites)
-            login @user
-            url = if !FoodsoftConfig[:membership_fee].nil? and FoodsoftConfig[:ordergroup_approval_payment]
-              FoodsoftSignup.payment_link self
-            else
-              root_url
-            end
-            redirect_to url, notice: I18n.t('signup.controller.notice')
-          end
-        end
-      rescue => e
-        flash[:error] = I18n.t('errors.general_msg', msg: e)
-      end
     else
-      @user.settings.defaults['profile']['language'] ||= session[:locale]
-      render 'login/accept_invitation', locals: {form_url: signup_path}
+      @user = User.new(params[:user])
+      if request.post?
+        begin
+          # XXX code-duplication from LoginController#accept_invitation
+          User.transaction do
+            # enforce group (security!)
+            @user.ordergroup = {id: 'new'}
+            # save!
+            if @user.save
+              session[:locale] = @user.locale
+              # but we proceed slightly differently (TODO same behaviour for invites)
+              login @user
+              url = if !FoodsoftConfig[:membership_fee].nil? and FoodsoftConfig[:ordergroup_approval_payment]
+                FoodsoftSignup.payment_link self
+              else
+                root_url
+              end
+              redirect_to url, notice: I18n.t('signup.controller.notice')
+            end
+          end
+        rescue => e
+          flash[:error] = I18n.t('errors.general_msg', msg: e)
+        end
+      else
+        @user.settings.defaults['profile']['language'] ||= session[:locale]
+        render 'login/accept_invitation', locals: {form_url: signup_path}
+      end
     end
   end
 
