250a9f7ba37f7133ca07c6687e28d9caef9cf0cd
wvengen <dev-voko@willem.engen.nl>
2014-03-27 12:43:53 +0100

    add payorder spec

--- spec/models/payorder_spec.rb
+++ spec/models/payorder_spec.rb
@@ -77,14 +77,13 @@ if defined? FoodsoftPayorder
           expect([goa, goa2].map(&:reload).map(&:result)).to eq [1, 0]
         end
 
-        # might be nice, but this is not the case at the moment (0 result for both)
-        #it 'gets second article when paid only second' do
-        #  update_quantities goa3, 2, 0
-        #  update_quantities goa,  1, 0
-        #  credit go.ordergroup, article.fc_price
-        #  order.finish!(admin)
-        #  expect([goa, goa3].map(&:reload).map(&:result)).to eq [1, 0]
-        #end
+        it 'gets second article when paid only second' do
+          update_quantities goa3, 2, 0
+          update_quantities goa,  1, 0
+          credit go.ordergroup, article.fc_price
+          order.finish!(admin)
+          expect([goa, goa3].map(&:reload).map(&:result)).to eq [1, 0]
+        end
 
         it 'gets one article when not paid enough for two' do
           update_quantities goa,  1, 0
@@ -98,8 +97,25 @@ if defined? FoodsoftPayorder
       describe 'with multiple orders' do
         let(:article2) { create :article, unit_quantity: 1 }
         let(:order2)   { create :order, article_ids: [article2.id] }
-        let(:go2)      { create :group_order, order: order2 }
+        let(:go2)      { create :group_order, order: order2, ordergroup: go.ordergroup }
         let(:goa2)     { create :group_order_article, group_order: go2, order_article: order2.order_articles.first }
+
+        it 'has no result without funds' do
+          update_quantities goa, 1, 0
+          update_quantities goa2, 3, 0
+          order.finish!(admin)
+          order2.finish!(admin)
+          expect([goa, goa2].map(&:reload).map(&:result)).to eq [0, 0]
+        end
+
+        it 'has result with exact funds before ordering' do
+          credit go.ordergroup, article.fc_price*5 + article2.fc_price*3
+          update_quantities goa,  5, 0
+          update_quantities goa2, 3, 0
+          order.finish!(admin)
+          order2.finish!(admin)
+          expect([goa, goa2].map(&:reload).map(&:result)).to eq [5, 3]
+        end
 
         it 'gets first when paid only first' do
           update_quantities goa,  1, 0
