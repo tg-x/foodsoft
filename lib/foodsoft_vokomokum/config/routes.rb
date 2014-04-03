3ce4cdcb8a5d3fa03cb7650213b88031253f47c0
wvengen <dev-voko@willem.engen.nl>
2014-04-03 17:12:33 +0200

    fix strange routing error on one development box

--- lib/foodsoft_vokomokum/config/routes.rb
+++ lib/foodsoft_vokomokum/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   scope '/:foodcoop' do
     get '/login/vokomokum', controller: 'vokomokum', action: 'login'
-    namespace :finance do
-      get :vokomokum_export_amounts, controller: 'vokomokum', action: 'export_amounts'
-    end
+    get '/finance/vokomokum_export_amounts', controller: 'vokomokum', action: 'export_amounts'
   end
 end
