5a036a57989a21c46b759969eac469a655d60b6d
wvengen <dev-voko@willem.engen.nl>
2014-04-14 14:32:34 +0200

    add i18n for payorder plugin

--- lib/foodsoft_payorder/app/helpers/payorder_helper.rb
+++ lib/foodsoft_payorder/app/helpers/payorder_helper.rb
@@ -9,9 +9,10 @@ module PayorderHelper
       # TODO use method to get link, and also support external urls
       amount = -@ordergroup.get_available_funds
       return_to = group_order_path(@order_date || :current)
-      link = FoodsoftPayorder.payment_link self, amount: amount, title: 'Pay for your current orders', fixed: true, return_to: return_to
+      link = FoodsoftPayorder.payment_link self, amount: amount, fixed: true,
+               title: I18n.t('helpers.payorder.payment_prompt'), return_to: return_to
       cls = "payment-status-btn btn btn-primary #{options[:class]}"
-      link_to glyph('chevron-right')+' '+'Payment', link, options.merge({class: cls})
+      link_to glyph('chevron-right')+' '+I18n.t('helpers.payorder.payment'), link, options.merge({class: cls})
     end
   end
 end
