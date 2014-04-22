deb677e7a685eb9f63ece3ffed09deda7954ae6c
wvengen <dev-voko@willem.engen.nl>
2014-04-22 20:22:53 +0200

    add sub_name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenticate(params[:nick], params[:password])
-    if user
-      login user
+    # allow before_filter to override user
+    @user ||= User.authenticate(params[:nick], params[:password])
+    if @user
+      login @user
       if session[:return_to].present?
         redirect_to_url = session[:return_to]
         session[:return_to] = nil
