a790a9cdffca5b99a64c7dcfc7f0312f5205292c
wvengen <dev-voko@willem.engen.nl>
2014-05-01 19:53:10 +0200

    beautify article and group pdfs

--- app/documents/order_by_articles.rb
+++ app/documents/order_by_articles.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class OrderByArticles < OrderPdf
+  include OrdersHelper
 
   def filename
     I18n.t('documents.order_by_articles.filename', :name => @order.name, :date => @order.ends.to_date) + '.pdf'
@@ -14,25 +15,44 @@ class OrderByArticles < OrderPdf
     @order.order_articles.ordered.each do |order_article|
       rows = []
       dimrows = []
+      has_tolerance = (order_article.price.unit_quantity > 1)
       for goa in order_article.group_order_articles.ordered
         rows << [goa.group_order.ordergroup.name,
-                  "#{goa.quantity} + #{goa.tolerance}",
+                 goa.tolerance > 0 ? "#{goa.quantity} + #{goa.tolerance}" : goa.quantity,
                  goa.result,
-                 number_with_precision(goa.total_price(order_article), precision: 2)]
+                 number_to_currency(goa.total_price(order_article))]
         dimrows << rows.length if goa.result == 0
       end
       next if rows.length == 0
       rows.unshift I18n.t('documents.order_by_articles.rows') # table header
 
-      text "#{order_article.article.name} (#{order_article.article.unit} | #{order_article.price.unit_quantity.to_s} | #{number_with_precision(order_article.price.fc_price, precision: 2)})",
-           style: :bold, size: fontsize(10)
+      sum = order_article.group_orders_sum
+      rows << [I18n.t('documents.order_by_groups.sum'),
+               order_article.tolerance > 0 ? "#{order_article.quantity} + #{order_article.tolerance}" : order_article.quantity,
+               sum[:quantity],
+               nil] #number_to_currency(sum[:price])]
+
+      text "<b>#{order_article.article.name}</b> " +
+           "(#{order_article.article.unit}; #{number_to_currency order_article.price.fc_price}; " +
+           units_history_line(order_article, @order, plain: true) + ')',
+           size: fontsize(10), inline_format: true
       table rows, cell_style: {size: fontsize(8), overflow: :shrink_to_fit} do |table|
+        # borders
+        table.cells.borders = [:bottom]
+        table.cells.border_width = 0.02
+        table.cells.border_color = 'dddddd'
+        table.rows(0).border_width = 1
+        table.rows(0).border_color = '666666'
+        table.row(rows.length-2).border_width = 1
+        table.row(rows.length-2).border_color = '666666'
+        table.row(rows.length-1).borders = []
+
         table.column(0).width = 200
-        table.columns(1..3).align = :right
+        table.columns(1..2).align = :center
         table.column(2).font_style = :bold
-        table.cells.border_width = 1
-        table.cells.border_color = '666666'
-        table.rows(0).border_bottom_width = 2
+        table.columns(3).align = :right
+        table.columns(3).width = 60
+
         # dim rows which were ordered but not received
         dimrows.each { |ri| table.row(ri).text_color = '999999' }
       end
--- app/documents/order_by_groups.rb
+++ app/documents/order_by_groups.rb
@@ -18,37 +18,58 @@ class OrderByGroups < OrderPdf
       dimrows = []
 
       group_order_articles = group_order.group_order_articles.ordered
+      has_tolerance = !group_order_articles.select {|goa| goa.order_article.price.unit_quantity > 1}.empty?
+
       group_order_articles.each do |goa|
         price = goa.order_article.price.fc_price(group_order.ordergroup)
         sub_total = price * goa.result
         total += sub_total
         rows <<  [goa.order_article.article.name,
-                  "#{goa.quantity} + #{goa.tolerance}",
-                  goa.result,
-                  number_with_precision(price, precision: 2),
-                  goa.order_article.price.unit_quantity,
                   goa.order_article.article.unit,
-                  number_with_precision(sub_total, precision: 2)]
+                  goa.tolerance > 0 ? "#{goa.quantity} + #{goa.tolerance}" : goa.quantity,
+                  goa.result,
+                  number_to_currency(price),
+                  number_to_currency(sub_total),
+                  (goa.order_article.price.unit_quantity if has_tolerance)]
         dimrows << rows.length if goa.result == 0
       end
       next if rows.length == 0
-      rows << [ I18n.t('documents.order_by_groups.sum'), nil, nil, nil, nil, nil, number_with_precision(total, precision: 2)]
-      rows.unshift I18n.t('documents.order_by_groups.rows') # Table Header
+
+      # total
+      rows << [{content: I18n.t('documents.order_by_groups.sum'), colspan: 5}, number_to_currency(total), nil]
+
+      # table header
+      rows.unshift I18n.t('documents.order_by_groups.rows').dup
+      if has_tolerance
+        rows.first[6] = {image: "#{Rails.root}/app/assets/images/package-bg.png", scale: 0.6, position: :center}
+      else
+        rows.first[6] = nil
+      end
 
       text group_order.ordergroup.name, size: fontsize(9), style: :bold
       table rows, width: 500, cell_style: {size: fontsize(8), overflow: :shrink_to_fit} do |table|
         # borders
-        table.cells.borders = []
-        table.row(0).borders = [:bottom]
-        table.row(group_order_articles.size).borders = [:bottom]
-        table.cells.border_width            = 1
-        table.cells.border_color            = '666666'
+        table.cells.borders = [:bottom]
+        table.cells.border_width = 0.02
+        table.cells.border_color = 'dddddd'
+        table.rows(0).border_width = 1
+        table.rows(0).border_color = '666666'
+        table.row(rows.length-2).border_width = 1
+        table.row(rows.length-2).border_color = '666666'
+        table.row(rows.length-1).borders = []
 
-        table.column(0).width = 240
-        table.column(2).font_style = :bold
-        table.columns(1..4).align = :right
-        table.column(6).align = :right
-        table.column(6).font_style = :bold
+        table.column(0).width = 190
+        table.column(3).font_style = :bold
+        table.columns(2..5).align = :center
+        table.column(4).align = :right
+        table.column(5).align = :right
+        table.column(5).font_style = :bold
+        table.column(6).align = :center
+        # dim rows not relevant for members
+        table.column(2).text_color = '999999'
+        table.column(6).text_color = '999999'
+        # hide unit_quantity if there's no tolerance anyway
+        table.column(6).width = has_tolerance ? 25 : 0
 
         # dim rows which were ordered but not received
         dimrows.each { |ri| table.row(ri).text_color = '999999' }
