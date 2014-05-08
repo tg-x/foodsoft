6d389e8ca777979a93c8d26194a2fe7423938eb4
wvengen <dev-voko@willem.engen.nl>
2014-05-08 12:48:04 +0200

    make financial transactions csv export more compatible with Microsoft Excel

--- lib/financial_transactions_csv.rb
+++ lib/financial_transactions_csv.rb
@@ -1,40 +1,29 @@
 require 'csv'
 
-class FinancialTransactionsCsv
+class FinancialTransactionsCsv < RenderCSV
   include ApplicationHelper
-  include ActionView::Helpers::NumberHelper
 
-  def initialize(financial_transactions, options={})
-    @financial_transactions = financial_transactions
+  def header
+    [
+      FinancialTransaction.human_attribute_name(:created_on),
+      FinancialTransaction.human_attribute_name(:ordergroup),
+      FinancialTransaction.human_attribute_name(:ordergroup),
+      FinancialTransaction.human_attribute_name(:user),
+      FinancialTransaction.human_attribute_name(:note),
+      FinancialTransaction.human_attribute_name(:amount)
+    ]
   end
 
-  def to_csv
-    CSV.generate do |csv|
-      # header
-      csv << [
-               FinancialTransaction.human_attribute_name(:created_on),
-               FinancialTransaction.human_attribute_name(:ordergroup),
-               FinancialTransaction.human_attribute_name(:ordergroup),
-               FinancialTransaction.human_attribute_name(:user),
-               FinancialTransaction.human_attribute_name(:note),
-               FinancialTransaction.human_attribute_name(:amount)
-             ]
-      # data
-      @financial_transactions.includes(:user, :ordergroup).each do |t|
-        csv << [
-                 t.created_on,
-                 t.ordergroup_id,
-                 t.ordergroup.name,
-                 show_user(t.user),
-                 t.note,
-                 number_to_currency(t.amount)
-               ]
-      end
+  def data
+    @object.includes(:user, :ordergroup).each do |t|
+      yield [
+              t.created_on,
+              t.ordergroup_id,
+              t.ordergroup.name,
+              show_user(t.user),
+              t.note,
+              number_to_currency(t.amount)
+            ]
     end
-  end
-
-  # Helper method to test pdf via rails console: OrderCsv.new(order).save_tmp
-  def save_tmp
-    File.open("#{Rails.root}/tmp/#{self.class.to_s.underscore}.csv", 'w') {|f| f.write(to_csv.force_encoding("UTF-8")) }
   end
 end
